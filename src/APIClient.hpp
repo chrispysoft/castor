61aea34fc84381e270e7f19ef7751cae8b049ea2
Chris Pastl <christoph.pastl@fro.at>
2024-12-06 01:46:26 +0100

    add curl and api client

---
+++ src/APIClient.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <curl/curl.h>
+#include <iostream>
+#include <string>
+#include "util.hpp"
+
+namespace lap {
+class APIClient {
+    static constexpr const char* kPlaylogURL = "http://localhost:8008/api/v1/playlog";
+    CURL* mCURL;
+
+public:
+
+    APIClient() {
+        mCURL = curl_easy_init();
+        if (!mCURL) {
+            throw std::runtime_error("Failed to init curl");
+        }
+        // curl_global_init(CURL_GLOBAL_ALL); // init Winsock
+    }
+
+    ~APIClient() {
+        if (mCURL) curl_easy_cleanup(mCURL);
+        // curl_global_cleanup();
+    }
+
+
+    void postPlaylog(const std::string tPlaylog) {
+        
+        curl_easy_setopt(mCURL, CURLOPT_URL, kPlaylogURL);
+        curl_easy_setopt(mCURL, CURLOPT_POSTFIELDS, tPlaylog.c_str());
+
+        auto res = curl_easy_perform(mCURL);
+        if (res == CURLE_OK) {
+            std::cout << "APIClient post success" << std::endl;
+        } else {
+            std::cerr << "APIClient post failed: " << curl_easy_strerror(res) << std::endl;
+        }
+    }
+};
+}
