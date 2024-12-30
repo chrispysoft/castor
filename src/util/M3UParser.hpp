592b3315843b67cbfecd60ab1cd5aa447a70cc3a
Chris Pastl <christoph.pastl@fro.at>
2024-12-30 02:50:24 +0100

    Revert "Revert "ref m3u parsing""
    This reverts commit 8a8da6ecbed98beb5fd87f21a7aa74d684634a6c.

--- src/util/M3UParser.hpp
+++ src/util/M3UParser.hpp
@@ -6,32 +6,34 @@ namespace cst {
 class M3UParser {
 public:
 
-    std::unordered_map<std::string, std::vector<PlayItem>> mMap = {};
+    std::unordered_map<size_t, std::vector<PlayItem>> mMap = {};
 
     void reset() {
         mMap.clear();
     }
 
-    std::vector<PlayItem> parse(const std::string& uri, time_t startOffset = 0) {
-        auto it = mMap.find(uri);
+    std::vector<PlayItem> parse(const std::string& url, const time_t& startTime = 0, const time_t& endTime = 0) {
+        auto hash = std::hash<std::string>{}(std::string(url + std::to_string(startTime) + std::to_string(endTime)));
+        auto it = mMap.find(hash);
         if (it != mMap.end()) {
             return it->second;
         } else {
-            auto items = _parse(uri);
-            mMap[uri] = items;
+            auto items = _parse(url);
+            mMap[hash] = items;
             return items;
         }
     }
 
-    std::vector<PlayItem> _parse(const std::string& uri) {
+    std::vector<PlayItem> _parse(const std::string& url, const time_t& startTime = 0, const time_t& endTime = 0) {
         // std::cout << "_parse " << uri << std::endl;
         using namespace std;
         std::vector<PlayItem> items;
-        ifstream file(uri);
+        ifstream file(url);
         if (!file.is_open()) {
-            throw runtime_error("Failed to open file " + uri);
+            throw runtime_error("Failed to open file " + url);
         }
         // log.debug() << "M3UParser opened " + uri;
+        auto itmStart = startTime;
         string line;
         while (getline(file, line)) {
             if (line.starts_with("#EXTINF:")) {
@@ -45,13 +47,17 @@ public:
                 string path;
                 if (getline(file, path)) {
                     util::stripM3ULine(path);
-                    items.push_back({0, duration, path});
+                    auto itmEnd = itmStart + duration;
+                    if (endTime > 0 && itmEnd < endTime) {
+                        items.push_back({itmStart, itmEnd, path});
+                    } else {
+                        log.warn() << "M3U item exceeds end time - skipping";
+                        break;
+                    }
                 }
             }
             // std::cout << line << std::endl;
         }
-
-        mMap[uri] = items;
 
         return items;
     }
